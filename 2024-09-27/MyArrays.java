86ccf17bd0735a101a255b6c7697f892a23b69fa
James Lei <bestpsyduckever@gmail.com>
2024-09-27 10:48:29 -0400

    Added arrayToString()

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,13 @@
+public class MyArrays {
+  public static String arrayToString(int[] nums) {
+    String result = "[";
+    for (int i = 0; i < nums.length; i++) {
+      result += nums[i];
+      if (i < nums.length - 1) result += ", ";
+    }
+    return result + "]";
+  }
+  public static void main(String[] args) {
+    System.out.println(arrayToString(new int[]{1, 2, 3}));
+  }
+}
