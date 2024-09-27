8237b5f478cbbb03af36609195f97972b389e376
James Lei <bestpsyduckever@gmail.com>
2024-09-27 10:58:37 -0400

    Added test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -7,7 +7,21 @@ public class MyArrays {
     }
     return result + "]";
   }
+  public static int[] returnCopy(int[] ary) {
+
+  }
+  public static int[] concatArray(int[] ary, int[] ary2) {
+
+  }
   public static void main(String[] args) {
-    System.out.println(arrayToString(new int[]{1, 2, 3}));
+    int[] arr = new int[] {1, 2, 3};
+    int[] copy = returnCopy(arr);
+    System.out.println("returnCopy() Test Case");
+    System.out.println("Items match? Expected " + arrayToString(arr) + " Returned " + arrayToString(copy));
+    System.out.println("Arrays are the same? Expected not " + arr + " Returned " + copy);
+    int[] arr2 = new int[] {4, 5, 6}
+    int[] concat = concatArray(arr, arr2);
+    System.out.println("concatArray() Test Case");
+    System.out.println("Expected a concationation of " + arrayToString(arr) +_"and" + arrayToString(arr2) + " Returned " arrayToString(concat));
   }
 }
