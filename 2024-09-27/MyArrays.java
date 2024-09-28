8920ff9d6cee9dda90b3e2f242d845c6fe996886
James Lei <bestpsyduckever@gmail.com>
2024-09-28 06:57:29 -0400

    Added concatArray() and more test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,22 +9,35 @@ public class MyArrays {
   }
   public static int[] returnCopy(int[] ary) {
     int[] result = new int[ary.length];
-    for (int i = 0; i < ary.length; i++) {
-      result[i] = ary[i];
-    }
+    for (int i = 0; i < ary.length; i++) result[i] = ary[i];
     return result;
   }
-  // public static int[] concatArray(int[] ary, int[] ary2) {
-  // }
+  public static int[] concatArray(int[] ary1, int[] ary2) {
+    int[] result = new int[ary1.length + ary2.length];
+    for (int i = 0; i < ary1.length; i++) result[i] = ary1[i];
+    for (int i = 0; i < ary2.length; i++) result[i + ary1.length] = ary2[i];
+    return result;
+  }
   public static void main(String[] args) {
     int[] arr = new int[] {1, 2, 3};
     int[] copy = returnCopy(arr);
-    System.out.println("returnCopy() Test Case");
+    System.out.println("returnCopy() Test Cases");
     System.out.println("Items match? Expected " + arrayToString(arr) + " Returned " + arrayToString(copy));
     System.out.println("Arrays are the same? Expected not " + arr + " Returned " + copy);
-    // int[] arr2 = new int[] {4, 5, 6}
-    // int[] concat = concatArray(arr, arr2);
-    // System.out.println("concatArray() Test Case");
-    // System.out.println("Expected a concationation of " + arrayToString(arr) +_"and" + arrayToString(arr2) + " Returned " arrayToString(concat));
+    arr = new int[] {};
+    copy = returnCopy(arr);
+    System.out.println("Items match? Expected " + arrayToString(arr) + " Returned " + arrayToString(copy));
+    System.out.println("Arrays are the same? Expected not " + arr + " Returned " + copy);
+    arr = new int[] {4, 3, 1, 4, 5};
+    copy = returnCopy(arr);
+    System.out.println("Items match? Expected " + arrayToString(arr) + " Returned " + arrayToString(copy));
+    System.out.println("Arrays are the same? Expected not " + arr + " Returned " + copy);
+    int[] arr2 = new int[] {4, 5, 6};
+    int[] concat = concatArray(arr, arr2);
+    System.out.println("concatArray() Test Cases");
+    System.out.println("Expected a concationation of " + arrayToString(arr) + " and " + arrayToString(arr2) + " Returned " + arrayToString(concat));
+    arr2 = new int[] {};
+    concat = concatArray(arr, arr2);
+    System.out.println("Expected a concationation of " + arrayToString(arr) + " and " + arrayToString(arr2) + " Returned " + arrayToString(concat));
   }
 }
