f5a55e10ce0824ac3327951e6470638c2b4ec351
James Lei <bestpsyduckever@gmail.com>
2024-10-31 10:59:02 -0400

    Added TriangleTester.isTriangle(int int int)

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -3,6 +3,13 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriangleTester {
+  public static boolean isTriangle(int a, int b, int c) {
+    if (a + b < c) return false;
+    if (a + c < b) return false;
+    if (b + c < a) return false;
+    return true;
+  }
+
   public static int countTrianglesA(String filename) {
     File file = new File(filename);
     Scanner input = new Scanner(file);
