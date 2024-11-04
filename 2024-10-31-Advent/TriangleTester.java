1c9f7cfbee4fb595cc62b5fc03c1ebe781a6af7c
James Lei <bestpsyduckever@gmail.com>
2024-11-04 10:25:22 -0500

    Efficency upgrades

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -4,26 +4,31 @@ import java.util.Scanner;
 
 public class TriangleTester {
   public static boolean isTriangle(int a, int b, int c) {
-    if (a + b < c) return false;
-    if (a + c < b) return false;
-    if (b + c < a) return false;
+    if (a + b <= c) return false;
+    if (a + c <= b) return false;
+    if (b + c <= a) return false;
     return true;
   }
 
   public static int countTrianglesA(String filename) {
-    File file = new File(filename);
-    Scanner input = new Scanner(file);
-    int[] sides = new int[3]();
     int count = 0;
-    while(input.hasNextLine()){
-      for (int i = 0; i < 3; i++) {
-        sides[i] = Integer.parseInt(input.next())
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while(input.hasNext()){
+        if (isTriangle(input.nextInt(), input.nextInt(), input.nextInt())) {
+          count++;
+        }
       }
-      if (isTriangle(sides[0], sides[1], sides[2])) {
-        count++;
-      }
+      input.close();
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return 0;
     }
-    input.close();
     return count;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(countTrianglesA("inputTri.txt"));
   }
 }
