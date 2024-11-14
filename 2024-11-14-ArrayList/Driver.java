1669744645424c6b77c9fa841a89217a20d3aa1f
James Lei <bestpsyduckever@gmail.com>
2024-11-14 11:58:34 -0500

    Added test cases for ArrayListPractice.makeReversedList(ArrayList)

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -11,18 +11,30 @@ public class Driver {
     ArrayList<String> otherArrList = ArrayListPractice.createRandomArray(20);
     System.out.println(otherArrList);
     System.out.println(ArrayListPractice.mixLists(newArrList, otherArrList));
+
     int count = 10;
     ArrayList<Integer> expectedIndices = new ArrayList<Integer>();
+    System.out.print("Testing ArrayListPractice.replaceEmpty(ArrayList): ");
     for (int n = 0; n < 10; n++) {
       expectedIndices = new ArrayList<Integer>();
-      newArrList = ArrayListPractice.createRandomArray((int) (Math.random() * 2000000));
-      otherArrList = ArrayListPractice.createRandomArray((int) (Math.random() * 2000000));
+      newArrList = ArrayListPractice.createRandomArray((int) (Math.random() * 200000));
       for (int i = 0; i < newArrList.size(); i++) {
         if (newArrList.get(i).equals("")) expectedIndices.add(i);
       }
       ArrayListPractice.replaceEmpty(newArrList);
       for (int i : expectedIndices) {
         if (! newArrList.get(i).equals("Empty")) count--;
+      }
+    }
+    System.out.println(count == 10 ? "PASSED" : "FAILED");
+
+    count = 10;
+    System.out.print("Testing ArrayListPractice.makeReversedList(ArrayList): ");
+    for (int n = 0; n < 10; n++) {
+      newArrList = ArrayListPractice.createRandomArray((int) (Math.random() * 200000));
+      otherArrList = ArrayListPractice.makeReversedList(newArrList);
+      for (int i = 0; i < newArrList.size(); i++) {
+        if (newArrList.get(i) != otherArrList.get(newArrList.size() - 1 -i)) count--;
       }
     }
     System.out.println(count == 10 ? "PASSED" : "FAILED");
