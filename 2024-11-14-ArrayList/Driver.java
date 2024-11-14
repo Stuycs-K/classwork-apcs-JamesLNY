5db4829e466ec880d3bb1b5fca1b75b5b29ec982
James Lei <bestpsyduckever@gmail.com>
2024-11-14 12:06:09 -0500

    Added testing for ArrayListPractice.mixLists(ArrayList, ArrayList)

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Driver {
   public static void main(String[] args) {
+    System.out.println("Testing ArrayListPractice General Methods: ");
     ArrayList<String> newArrList = ArrayListPractice.createRandomArray(10);
     System.out.println(newArrList);
     ArrayListPractice.replaceEmpty(newArrList);
@@ -35,6 +36,20 @@ public class Driver {
       otherArrList = ArrayListPractice.makeReversedList(newArrList);
       for (int i = 0; i < newArrList.size(); i++) {
         if (newArrList.get(i) != otherArrList.get(newArrList.size() - 1 -i)) count--;
+      }
+    }
+    System.out.println(count == 10 ? "PASSED" : "FAILED");
+
+    count = 10;
+    ArrayList<String> moreArrayList;
+    System.out.print("Testing ArrayListPractice.mixLists(ArrayList, ArrayList): ");
+    for (int n = 0; n < 10; n++) {
+      newArrList = ArrayListPractice.createRandomArray((int) (Math.random() * 200000));
+      otherArrList = ArrayListPractice.createRandomArray((int) (Math.random() * 200000));
+      moreArrayList = ArrayListPractice.mixLists(newArrList, otherArrList);
+      for (int i = 0; i < Math.min(newArrList.size(), otherArrList.size()); i++) {
+        if (moreArrayList.get(i * 2) != newArrList.get(i)) count--;
+        if (moreArrayList.get(i * 2 + 1) != otherArrList.get(i)) count--;
       }
     }
     System.out.println(count == 10 ? "PASSED" : "FAILED");
