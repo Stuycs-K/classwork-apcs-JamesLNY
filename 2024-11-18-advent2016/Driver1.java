4f34fc04ef3b1f0e27371d585785080b60760f7d
James Lei <bestpsyduckever@gmail.com>
2024-11-18 10:47:46 -0500

    Solved Problem 1

---
+++ 2024-11-18-advent2016/Driver1.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Driver1 {
+  public static void main(String[] args) {
+    int xChange = 0, yChange = 0, direction = 1;
+    String stringInput;
+    try {
+      File file = new File("Input1.txt");
+      Scanner input = new Scanner(file);
+      while (input.hasNext()) {
+        stringInput = input.next();
+        if (stringInput.charAt(0) == 'L') {
+          direction++;
+          if (direction == 4) direction = 0;
+        } else {
+          direction--;
+          if (direction == -1) direction = 3;
+        }
+        stringInput = stringInput.substring(1, stringInput.length() - 1);
+        switch (direction) {
+          case 0:
+            xChange += Integer.parseInt(stringInput);
+            break;
+          case 1:
+            yChange += Integer.parseInt(stringInput);
+            break;
+          case 2:
+            xChange -= Integer.parseInt(stringInput);
+            break;
+          case 3:
+            yChange -= Integer.parseInt(stringInput);
+        }
+      }
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+    System.out.println(Math.abs(xChange) + Math.abs(yChange));
+  }
+}
