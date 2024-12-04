31784d6fded475215e126b8317d58ce714c12bd9
James Lei <bestpsyduckever@gmail.com>
2024-12-04 12:38:29 -0500

    Added numbers

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -10,13 +10,32 @@ public class Driver {
   public static void main(String[] args) {
     System.out.print(Text.CLEAR_SCREEN);
     System.out.print(Text.HIDE_CURSOR);
-    Text.go(0, 0);
+    // PART 1
     for (int i = 0; i < 80; i++) {
       for (int n = 0; n < 30; n++) {
         if (n == 0 || n == 29 || i == 0 || i == 79) colorSquare(i, n, getRandomColor());
       }
     }
-    Text.go(30, 0);
+    // PART 2
+    System.out.print(Text.RESET);
+    int[] randomNumbers = new int[3];
+    for (int i = 0; i < 3; i++) {
+      randomNumbers[i] = (int) (Math.random() * 100);
+      Text.go(2, 20 + i * 20);
+      if (randomNumbers[i] < 25) {
+        Text.color(Text.RED + 60);
+      } else if (randomNumbers[i] > 75) {
+        Text.color(Text.GREEN + 60);
+      } else {
+        Text.color(Text.WHITE);
+      }
+      System.out.print(randomNumbers[i]);
+    }
+    // Part 3
+    for (int i = 2; i < 79; i++) {
+      colorSquare(i, 3, Text.BLUE + 60);
+    }
+    Text.go(31, 0);
     System.out.print(Text.RESET);
   }
 }
