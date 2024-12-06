83c3c92185661f2165357cc19d19ea3e6afd3d06
James Lei <bestpsyduckever@gmail.com>
2024-12-06 11:00:57 -0500

    Added Wizard.specialAttack(Adventurer)

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -20,7 +20,7 @@ public class Wizard extends Adventurer {
   public void setSpecial(int n) {
     mana = n;
   }
-  public int getSpecialMan() {
+  public int getSpecialMax() {
     return maxMana;
   }
   public String attack(Adventurer other) {
@@ -41,6 +41,25 @@ public class Wizard extends Adventurer {
       return getName() + " drank a magic potion, which increased their max HP by 1!";
     } else {
       return getName() + " accidentally drank water, which obviously didn't do anything!";
+    }
+  }
+  public String specialAttack(Adventurer other) {
+    if (getSpecial < 10) return getName() + " doesn't have enough mana to cast their special.";
+    mana = 0;
+    int random = (int) (Math.random() * 3);
+    switch (random) {
+      case 0:
+        other.setHP(1);
+        return getName() + " consumed 10 mana to summon a tornado on top of " + other.getName() + ", reducing them to 1 HP!"
+      case 1:
+        int change = Math.min(1, other.getmaxHP() - 5);
+        other.setmaxHP(change);
+        other.setHP(Math.min(other.getMaxHP(), other.getHP()));
+        return getName() + " consumed 10 mana to throw a lightning bolt at " + other.getName() + ", which reduced their max HP to " + change;
+      default:
+        other.setmaxHP(2);
+        other.setHP(2)''
+        return getName() + " consumed 10 mana to turn " + other.getName() + " into a duck, setting both their max HP and HP to 2!"
     }
   }
 }
