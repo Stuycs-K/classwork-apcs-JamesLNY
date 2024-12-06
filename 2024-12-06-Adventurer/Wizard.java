8ac2c591ca5b11be035032de07b309c14ff30247
James Lei <bestpsyduckever@gmail.com>
2024-12-06 10:30:56 -0500

    Added Wizard.getSpecialMax()

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,12 +1,15 @@
 public class Wizard extends Adventurer {
   private int mana;
+  private int maxMana
   public Wizard(String name) {
     super(name, 10);
     mana = 10;
+    this.maxMana = 10;
   }
   public Wizard(String name, int hp, int mana) {
     super(name, hp);
     this.mana = mana;
+    this.maxMana = mana;
   }
   public String getSpecialName() {
     return "mana";
@@ -16,5 +19,8 @@ public class Wizard extends Adventurer {
   }
   public void setSpecial(int n) {
     mana = n;
+  }
+  public int getSpecialMan() {
+    return maxMana;
   }
 }
